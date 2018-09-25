e201ec9f125a9df11f7b50deeb0e1e4c4cede2e5
Harry Ganz <harryyehudah.ganz@gartner.com>
2018-09-25 16:38:10 -0400

    Changes database config to use env variable

--- server.js
+++ server.js
@@ -3,18 +3,4 @@ const citiesDb = require('./src/db/cities');
 const mockCities = require('./src/fixtures/cities');
 
 const port = process.env.PORT || 3000;
-citiesDb.create()
-  .then(() => citiesDb.insert(mockCities(100)))
-  .then(() => {
-    app.listen(port);
-  });
-
-function cleanUp() {
-  console.log('cleaning up');
-  citiesDb.drop().then(() => {
-    process.exit();
-  });
-}
-
-process.on('SIGINT', cleanUp);
-process.on('SIGHUP', cleanUp);
+app.listen(port);
--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,5 @@ const knex = require('knex');
 
 module.exports = knex({
   client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'citiesusr',
-    password: 'citiesusr',
-    database: 'cities_test',
-  },
+  connection: process.env.PG_CONNECTION_STRING,
 });
